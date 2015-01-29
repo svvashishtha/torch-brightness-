1b216586885e52a5d9bbc5d46cd042a8f39498a8
saurabh vashisht <svvashishtha@gmail.com>
2015-01-29 12:04:16 +0530

    Update MainActivity.java
    removed code for flash device.
    No device to test for flash access.

--- app/src/main/java/com/example/saurabh/brightness/MainActivity.java
+++ app/src/main/java/com/example/saurabh/brightness/MainActivity.java
@@ -2,25 +2,12 @@ package com.example.saurabh.brightness;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.hardware.Camera;
-import android.hardware.Camera.Parameters;
 import android.os.Bundle;
 import android.provider.Settings;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.SeekBar;
 import android.widget.ToggleButton;
 
-import java.security.Policy;
-
-
 public class MainActivity extends Activity {
-
-    private Camera camera;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,31 +19,12 @@ public class MainActivity extends Activity {
             public void onClick(View v) {
                     boolean state = button.isChecked();
                 if(state) {
-                    if(getApplicationContext().getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH))
-                    {
-                        Camera.Parameters p = camera.getParameters();
-                        PackageManager pm=getApplicationContext().getPackageManager();
-                        p.setFlashMode(Parameters.FLASH_MODE_TORCH);
-                        camera.setParameters(p);
-                        camera.startPreview();
-
-                    }
-                    else {
                         Intent intent = new Intent(MainActivity.this, torch.class);
                         MainActivity.this.startActivity(intent);
-                    }
                 }
                 button.setChecked(!state);
             }
         });
-
-
-
-
-
-
-
-
     }
     @Override
     public void onDestroy()
